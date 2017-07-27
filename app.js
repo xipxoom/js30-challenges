e21c4b2df807383fdf76ba8b7372ba918fb88237
George Dhoore <georgiepoo@gmail.com>
2017-07-27 16:07:33 -0400

    All functionality working

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-/*jshint browser: true, devel: true*/
+/*jshint esnext: true, browser: true, devel: true*/
 
+// Wrapper should not be necessary with the defer attribute
 document.addEventListener('DOMContentLoaded', function() {
+  function handleUpdate() {
+    var suffix = '';
+    if (this.name === 'spacing' || this.name === 'blur') {
+      suffix = 'px';
+    }
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  }
   
+  const controls = document.querySelectorAll('.controls input');
+  for (const control of controls) {
+    control.addEventListener('change', handleUpdate);
+    control.addEventListener('mousemove', handleUpdate);
+  }
 });
